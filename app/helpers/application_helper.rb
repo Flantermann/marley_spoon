90228584e8e8ffd78bd2f759a1507001fc54fb96
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-07-13 16:28:40 +0200

    change in  markdown renderer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,17 +6,10 @@ module ApplicationHelper
         filter_html:     true,
         hard_wrap:       true,
         link_attributes: { rel: 'nofollow', target: "_blank" },
-        space_after_headers: true,
-        fenced_code_blocks: true
+        space_after_headers: true
     }
-    extensions = {
-        autolink:           true,
-        superscript:        true,
-        disable_indented_code_blocks: true
-    }
-
     renderer = ::Redcarpet::Render::HTML.new(options)
-    markdown = ::Redcarpet::Markdown.new(renderer, extensions)
+    markdown = ::Redcarpet::Markdown.new(renderer)
     markdown.render(text).html_safe
   end
 end
